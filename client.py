3b30ebacb4527a32ce7ed71d53cbce4ca0670856
vekt0R-HUB <zealot.algo@gmail.com>
2023-12-09 11:54:24 -0500

    Moved checksum to a separate file since it was a common function

--- client.py
+++ client.py
@@ -1,26 +1,6 @@
+from checksum_utils import udp_checksum
 import socket
 import struct
-
-
-# Helper methods
-def udp_checksum(source_ip, dest_ip, udp_packet):
-    pseudo_header = struct.pack('!4s4sBBH',
-                                socket.inet_aton(source_ip),
-                                socket.inet_aton(dest_ip),
-                                0,
-                                socket.IPPROTO_UDP,
-                                len(udp_packet))
-    return checksum(pseudo_header + udp_packet)
-
-
-def checksum(packet):
-    if len(packet) % 2 != 0:
-        packet += b'\0'
-    res = sum((int.from_bytes(packet[i:i + 2], 'big') for i in range(0, len(packet), 2)))
-    res = (res >> 16) + (res & 0xffff)
-    res += res >> 16
-    return ~res & 0xffff
-
 
 # Define server address and port
 SERVER_ADDRESS = "127.0.0.1"
--- vpn.py
+++ vpn.py
@@ -1,25 +1,6 @@
+from checksum_utils import udp_checksum
 import socket
 import struct
-
-
-# Helper methods
-def udp_checksum(source_ip, dest_ip, udp_packet):
-    pseudo_header = struct.pack('!4s4sBBH',
-                                socket.inet_aton(source_ip),
-                                socket.inet_aton(dest_ip),
-                                0,
-                                socket.IPPROTO_UDP,
-                                len(udp_packet))
-    return calc_checksum(pseudo_header + udp_packet)
-
-
-def calc_checksum(packet) -> int:
-    if len(packet) % 2 != 0:
-        packet += b'\0'
-    res = sum((int.from_bytes(packet[i:i + 2], 'big') for i in range(0, len(packet), 2)))
-    res = (res >> 16) + (res & 0xffff)
-    res += res >> 16
-    return ~res & 0xffff
 
 
 # Create and bind raw socket
