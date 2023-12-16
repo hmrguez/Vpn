df781f7549b19f300b153aa672a41b58fd0fe052
vekt0R-HUB <zealot.algo@gmail.com>
2023-12-16 15:57:09 -0500

    Fixed bug with stop and cleaned the UI

--- vpn_cli.py
+++ vpn_cli.py
@@ -7,11 +7,13 @@ vpn = VPN()
 
 def main():
     while True:
-        command = input("Enter command: ").split()
+        command = input("\033[92mVPN > \033[0m").split()
 
         if command[0] == 'start':
+            print("\033[93mStarting VPN...\033[0m")
             vpn.start()
         elif command[0] == 'stop':
+            print("\033[93mStopping VPN...\033[0m")
             vpn.stop()
         elif command[0] == 'create_user':
             vpn.create_user(command[1], command[2], command[3])
@@ -20,11 +22,12 @@ def main():
         elif command[0] == 'restrict_user':
             vpn.restrict_user(command[1])
         elif command[0] == 'show_logs':
+            print("\033[93mShowing logs...\033[0m")
             while not vpn.log_queue.empty():
-                print(vpn.log_queue.get())
+                print("\033[96m" + vpn.log_queue.get() + "\033[0m")
                 print("---------------------------------------------------------")
         else:
-            print("Invalid command")
+            print("\033[91mInvalid command\033[0m")
 
 
 if __name__ == "__main__":
