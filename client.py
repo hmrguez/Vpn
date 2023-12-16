c63236d269b5f15da74fa2246a7ff2fa80431be9
vekt0R-HUB <zealot.algo@gmail.com>
2023-12-16 16:12:16 -0500

    Complicated client sends 10 packets, where 1/5 doesn't match the checksum

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+import random
+
 from checksum_utils import udp_checksum
 import socket
 import struct
@@ -15,24 +17,37 @@ SOURCE_PORT = 63402
 
 # Define real destination port and message
 REAL_DEST_PORT = 7000
-MESSAGE = b"Hello from the client!"
 
-# Pack the real destination port before actual data
-data = struct.pack(">H", REAL_DEST_PORT) + MESSAGE
+# Build a string array of 10 strings that build up a coherent message
+messages = ["Hello", "from", "the", "client", "!", "How", "is", "it", "going", "?!"]
 
-# Build the UDP header
-udp_header = struct.pack("!HHHH", SOURCE_PORT, SERVER_PORT, 8 + len(data), 0)
+for i in range(len(messages)):
 
-# Calculate checksum
-udp_checksum = udp_checksum(SOURCE_ADDRESS, SERVER_ADDRESS, udp_header + data)
-udp_header = struct.pack("!HHHH", SOURCE_PORT, SERVER_PORT, 8 + len(data), udp_checksum)
+    message = messages[i].encode()
+    # Pack the real destination port before actual data
+    data = struct.pack(">H", REAL_DEST_PORT) + message
+
+    # Build the UDP header
+    udp_header = struct.pack("!HHHH", SOURCE_PORT, SERVER_PORT, 8 + len(data), 0)
+
+    # Calculate checksum
+    checksum = udp_checksum(SOURCE_ADDRESS, SERVER_ADDRESS, udp_header + data)
+
+    rand_num = random.randint(1, 5)
+    message = messages[i]
+    if rand_num == 1:
+        checksum_to_send = 1
+    else:
+        checksum_to_send = checksum
+
+    udp_header = struct.pack("!HHHH", SOURCE_PORT, SERVER_PORT, 8 + len(data), checksum_to_send)
+    packet = udp_header + data
+    sock.sendto(packet, (SERVER_ADDRESS, SERVER_PORT))
+    print(f"Sent UDP packet #{i} to {SERVER_ADDRESS}:{SERVER_PORT} with real destination port {REAL_DEST_PORT}")
 
 # Build the entire packet
-packet = udp_header + data
 
 # Send the packet to the server
-sock.sendto(packet, (SERVER_ADDRESS, SERVER_PORT))
-print(f"Sent UDP packet to {SERVER_ADDRESS}:{SERVER_PORT} with real destination port {REAL_DEST_PORT}")
 
 # Close the socket
 sock.close()
