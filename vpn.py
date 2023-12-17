0a3cd5a0ee360151e4958bf5ddad7e6f665fd869
Scorpion-Ceeb <sebastiansuarezgomez01@gmail.com>
2023-12-17 14:28:59 -0500

    VPN run method

--- vpn.py
+++ vpn.py
@@ -3,8 +3,11 @@ import queue
 import json
 import socket
 import threading
+import struct
 
 from utils import assign_ip_address
+from checksum_utils import udp_checksum
+
 
 
 
@@ -108,3 +111,68 @@ class VPN:
         # Write the message to the file
         with open('logs.txt', 'a') as f:
             f.write(message + '\n')
+    
+    def run(self):
+        while True:
+            try:
+                # Receive data
+                data, addr = self.raw_socket.recvfrom(65535)
+
+                # Unpack UDP header
+                udp_header = data[20:28]
+                udp_data = struct.unpack('!HHHH', udp_header)
+
+                # Extract information
+                source_port = udp_data[0]
+                dest_port = udp_data[1]
+                length = udp_data[2]
+
+                # Check if the packet matches the filter criteria
+                if dest_port == self.SERVER_PORT:
+
+                    # Sender port is always 0 because it sends directly through the interface
+                    sender_addr, sender_port = addr
+
+                    # Check if the user is created and not restricted
+                    if not self.validate_user(sender_addr, source_port):
+                        continue
+
+                    # Check checksum
+                    received_checksum = udp_data[3]
+
+                    # Set checksum field to zero before calculating checksum
+                    zero_checksum_header = udp_header[:6] + b'\x00\x00' + udp_header[8:]
+                    calculated_checksum = udp_checksum(sender_addr, self.SERVER_ADDRESS, zero_checksum_header + data[28:])
+
+                    if received_checksum != calculated_checksum:
+                        self.log_message("Checksum does not match, packet might be corrupted")
+                        continue  # Skip the rest of the loop and wait for the next packet
+
+                    # Extract destination port from data
+                    forward_port = struct.unpack('!H', data[28:30])[0]
+
+                    # Generate new UDP header with server port as source and dynamic port from data
+                    new_source_port = self.SERVER_PORT
+                    new_udp_header = struct.pack("!HHHH", new_source_port, forward_port, length, 0)
+
+                    # Calculate new checksum
+                    new_udp_checksum = udp_checksum(self.SERVER_ADDRESS, self.SERVER_ADDRESS, new_udp_header + data[28:])
+                    new_udp_header = struct.pack("!HHHH", new_source_port, forward_port, length, new_udp_checksum)
+
+                    # Combine new header and original data for forwarding
+                    forwarded_packet = new_udp_header + data[28:]
+
+                    # Send the forwarded packet
+                    self.raw_socket.sendto(forwarded_packet, (self.SERVER_ADDRESS, forward_port))
+
+                    # Print confirmation
+                    self.log_message(
+                        f"Forwarded packet coming from {sender_addr}:{source_port} to {self.SERVER_ADDRESS}:{forward_port}")
+                else:
+                    # Discard the packet
+                    self.log_message(f"Ignored packet coming from {source_port}")
+            except AttributeError:
+                if self.raw_socket is None:
+                    break
+                else:
+                    raise
